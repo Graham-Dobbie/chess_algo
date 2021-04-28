18841f4bc9ee72fa82142dfa7d93b234916cc637
Graham Dobbie <grahamabq@gmail.com>
2021-04-27 18:49:22 -0600

    idk what this is

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "chess.hpp"
+#include "tree.hpp"
 
-int main() {
-    std::cout << "Hello World!\n";
+using namespace chess;
+using namespace std;
+
+pair<Move, unordered_map<string>>  getMove(Board &board){
+
+    MoveGenerator moveGenerator = board.getMoveGenerator();
+    Move move = moveGenerator.nextMove();
+    
+    unordered_map<string> debug{
+        {"is this working?" , "yes"}
+    };
+
+    return  pair<move, debug>;
+}
+
+
+int main(string name, string apikey) {
+    
+    TreeHandler = tree.TreeHandler()
+
     return 0;
 }
